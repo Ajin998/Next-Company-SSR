6ca02948759c1dc56440ffa21ea27c8c054fe607
Ajin Varghese <ajin.varghese@contentstack.com>
2021-01-14 01:07:16 +0530

    Shifted styling from inline css module to scss module
    Only html tags are not included in scss since scss does not allow html tags so embedded inline css module styling to html elements like button and footer in contentstack and surfboard pages

--- pages/contentstack.js
+++ pages/contentstack.js
@@ -3,19 +3,20 @@ import Link from "next/link";
 import { BASE_URL, contentstack_data } from "../constants/endpoint";
 import fetchData from "../services/fetchData";
 import Footer from "../components/Footer";
+import styles from "../styles/Contentstack.module.scss";
 
 function contentstack(props) {
   return (
-    <div className="main-div">
-      <nav className="navbar">
+    <div className={styles["main-div"]}>
+      <nav className={styles["navbar"]}>
         <img
           src="https://camo.githubusercontent.com/56e71e3403b2c78b63007bf8f542f7b06c71f95542ae7154fe40f6a8b6b9e8a1/68747470733a2f2f63646e2e66732e746561636861626c6563646e2e636f6d2f72355937716a6271543036476a4d533451413057"
           alt="Contentstack_img"
           width="300"
         />
-        <div className="Nav-links">
+        <div className={styles["Nav-links"]}>
           <Link href="/contentstack">
-            <button className="Home-button">Home</button>
+            <button className={styles["Home-button"]}>Home</button>
           </Link>
           <Link href="/raw_engg">
             <button>Raw_Engineering</button>
@@ -26,89 +27,26 @@ function contentstack(props) {
         </div>
       </nav>
       {props.data.map((data) => (
-        <main className="main-content">
-          <img src={data.banner_img} className="banner-img" />
+        <main className={styles["main-content"]}>
+          <img src={data.banner_img} className={styles["banner-img"]} />
           <h1>{data.company_title}</h1>
           <p>{data.company_para}</p>
 
-          <button className="content-button">TRY FOR FREE</button>
+          <button className={styles["content-button"]}>TRY FOR FREE</button>
 
-          <a className="content-anchor"> Request a Demo</a>
+          <a className={styles["content-anchor"]}> Request a Demo</a>
         </main>
       ))}
       <footer>
         <Footer />
       </footer>
       <style jsx>{`
-        @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap");
-        .main-div {
-          // height: 100vh;
-          width: 100vw;
-          background-image: linear-gradient(
-            to left bottom,
-            #3c3c83,
-            #5a3b87,
-            #763788,
-            #903284,
-            #a82c7d
-          );
-        }
-        .navbar {
-          padding: 10px;
-          // border-bottom: 2px solid black;
-          display: flex;
-          justify-content: space-between;
-        }
-        .Nav-links {
-          width: 50%;
-          display: flex;
-          align-items: center;
-          justify-content: space-evenly;
-        }
         button {
           padding: 10px;
           border-radius: 20px;
           border: 1px solid black;
           cursor: pointer;
         }
-        .Home-button {
-          background-color: #ee5644;
-          width: 20%;
-          border-radius: 20px;
-          color: white;
-          border: 1px solid black;
-        }
-        .main-content {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          text-align: center;
-          color: white;
-          font-family: "Montserrat", sans-serif;
-        }
-        .banner-img {
-          width: 50%;
-          border: 2px solid black;
-          border-radius: 10px;
-          margin-top: 2rem;
-          box-shadow: 5px 10px 20px #9d64a9;
-        }
-        .main-content h1 {
-          font-size: 80px;
-        }
-        .main-content p {
-          font-weight: 300;
-        }
-        .content-button {
-          background-color: #ee5644;
-          color: white;
-          margin: 40px;
-        }
-        footer {
-          margin-top: 20px;
-          border-top: 2px solid black;
-        }
-        @media (max-width: );
       `}</style>
     </div>
   );
--- pages/surfboard.js
+++ pages/surfboard.js
@@ -3,19 +3,19 @@ import Link from "next/link";
 import { BASE_URL, surfboard_data } from "../constants/endpoint";
 import fetchData from "../services/fetchData";
 import Footer from "../components/Footer";
-
+import styles from "../styles/Surfboard.module.scss";
 function surfboard(props) {
   return (
-    <div className="main-div">
-      <nav className="navbar">
+    <div className={styles["main-div"]}>
+      <nav className={styles["navbar"]}>
         <img
           src="https://camo.githubusercontent.com/3dce3bc602f364173b522496e4d8239520c7da808298de30becd997ee2906f6f/68747470733a2f2f63646e2e66732e746561636861626c6563646e2e636f6d2f496d3765326f427a52634b304370466850363739"
           alt="surfboard_img"
           width="300"
         />
-        <div className="Nav-links">
+        <div className={styles["Nav-links"]}>
           <Link href="/surfboard">
-            <button className="Home-button">Home</button>
+            <button className={styles["Home-button"]}>Home</button>
           </Link>
           <Link href="/contentstack">
             <button>Contenstack</button>
@@ -26,70 +26,26 @@ function surfboard(props) {
         </div>
       </nav>
       {props.data.map((data) => (
-        <main className="main-content">
-          <img src={data.banner_img} className="banner-img" />
+        <main className={styles["main-content"]}>
+          <img src={data.banner_img} className={styles["banner-img"]} />
           <h1>{data.company_title}</h1>
           <p>{data.company_para}</p>
 
-          <button className="content-button">TRY FOR FREE</button>
+          <button className={styles["content-button"]}>TRY FOR FREE</button>
           <br />
 
-          <a className="content-anchor"> Request a Demo</a>
+          <a className={styles["content-anchor"]}> Request a Demo</a>
         </main>
       ))}
       <footer>
         <Footer />
       </footer>
       <style jsx>{`
-        @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap");
-        .navbar {
-          padding: 10px;
-          border-bottom: 1px solid black;
-          display: flex;
-          justify-content: space-between;
-        }
-        .Nav-links {
-          width: 50%;
-          display: flex;
-          align-items: center;
-          justify-content: space-evenly;
-        }
         button {
           padding: 10px;
           border-radius: 20px;
           border: 1px solid black;
           cursor: pointer;
-        }
-        .Home-button {
-          background-color: #2bcade;
-          width: 20%;
-          border-radius: 20px;
-          border: 1px solid black;
-        }
-        .main-content {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          background-image: url("https://images.assetsdelivery.com/compings_v2/olgaivolga/olgaivolga1512/olgaivolga151200160.jpg");
-          background-repeat: no-repeat;
-          background-size: cover;
-          font-family: "Montserrat", sans-serif;
-          padding: 5% 0;
-        }
-        .main-content h1 {
-          font-size: 50px;
-        }
-        .main-content p {
-          width: 80%;
-          text-align: center;
-        }
-        .banner-img {
-          width: 50%;
-          border-radius: 20px;
-        }
-        .content-button {
-          margin: 2%;
-          background-color: #55b3b1;
         }
         footer {
           background-color: #55b3b1;
